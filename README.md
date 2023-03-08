aa70be83ef2c1d97a3d2c86289247a8a36ec30ae
Piercen Helms <phelms1999@gmail.com>
2023-03-07 20:01:42 -0800

    added js and inquirer

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# readme-gen-new
+# readme-generator
+  [Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description 
+  This app will generate readmes for your GitHub repositories
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  The user should clone the repository from GitHub and download Node. Requires a file system and inquirer.
+  ## Usage 
+  Use inquirer from your command line to answer questions about your project
+  ## License 
+  This project is license under MIT
+  ## Contributing 
+  Contributors should read the installation section.
+  ## Tests
+  npm test
+  ## Questions
+  If you have any questions about this projects, please contact me directly at phelms1999@gmail.com. You can view more of my projects at https://github.com/reignbtw.
+  
---
+++ index.js
@@ -0,0 +1,153 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// link to page where readme is developed
+const generatePage = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    // uses inquirer to prompt questions to user
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false; 
+                }
+            }    
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false; 
+                }
+            }
+    
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false; 
+                }
+            }
+        }, 
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'GNU'],
+            default: ["MIT"],
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test', 
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'What does the user need to know about contributing to the repo?'
+        }
+    ]);
+};
+
+// TODO: Create a function to write README file
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+};
+
+// function call to initialize the program
+questions()
+// getting user answers
+.then(answers => {
+    return generatePage(answers);
+})
+// using data to display on the page
+.then(data => {
+    return writeFile(data);
+})
+
+.catch(err => {
+    console.log(err)
+})
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
